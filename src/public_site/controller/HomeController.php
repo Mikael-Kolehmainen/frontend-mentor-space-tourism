1e8f923768d5209964e7e54b862b7f3415b633f5
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2023-12-24 17:23:23 +0200

    Landing page for desktop complete

--- src/public_site/controller/HomeController.php
+++ src/public_site/controller/HomeController.php
@@ -8,13 +8,32 @@ class HomeController
   {
     echo "
         <link href='/src/public_site/styles/home.css' rel='stylesheet' type='text/css'>
+        <link href='/src/public_site/styles/hero.css' rel='stylesheet' type='text/css'>
       </head>
     ";
 
     $this->showHeader();
 
     echo "
-
+      <section>
+        <div class='hero'>
+          <div class='left'>
+            <h5>SO, YOU WANT TO TRAVEL TO</h5>
+            <h1>SPACE</h1>
+            <p>
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of it.
+              Well sit back, and relax because we’ll give you a truly out of this
+              world experience!
+            </p>
+          </div>
+          <div class='right'>
+            <div class='CTA'>
+              <h4>EXPLORE</h4>
+            </div>
+          </div>
+        </div>
+      </section>
     ";
   }
 
