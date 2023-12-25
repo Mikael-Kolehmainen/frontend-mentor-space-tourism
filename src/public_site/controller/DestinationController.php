0c78758e8b12fbf77992fd1d4770a186665f4d05
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2023-12-25 13:22:07 +0200

    Layout for destination page completed

--- src/public_site/controller/DestinationController.php
+++ src/public_site/controller/DestinationController.php
@@ -15,7 +15,40 @@ class DestinationController
 
     echo "
       <section>
-
+        <article>
+          <div class='title'>
+            01 <h5>PICK YOUR DESTINATION</h5>
+          </div>
+          <div class='planet'>
+            <img src='/src/public_site/media/image-moon.webp' alt='Moon'>
+          </div>
+        </article>
+        <article>
+          <nav>
+            <a href='/index.php/destination/moon' class='active'>MOON</a>
+            <a href='/index.php/destination/mars'>MARS</a>
+            <a href='/index.php/destination/europa'>EUROPA</a>
+            <a href='/index.php/destination/titan'>TITAN</a>
+          </nav>
+          <h2>MOON</h2>
+          <p>
+            See our planet as you’ve never seen it before. A perfect relaxing
+            rip away to help regain perspective and come back refreshed. While
+            you’re there, take in some history by visiting the Luna 2 and Apollo
+            11 landing sites.
+          </p>
+          <div class='line'></div>
+          <div class='facts-box'>
+            <div class='fact'>
+              <p class='subheading2'>AVG. DISTANCE</p>
+              <p class='subheading1'>384,400 KM</p>
+            </div>
+            <div class='fact'>
+              <p class='subheading2'>EST. TRAVEL TIME</p>
+              <p class='subheading1'>3 DAYS</p>
+            </div>
+          </div>
+        </article>
       </section>
     ";
   }
--- src/public_site/controller/HomeController.php
+++ src/public_site/controller/HomeController.php
@@ -15,22 +15,20 @@ class HomeController
     $this->showHeader();
 
     echo "
-      <section>
-        <div class='hero'>
-          <div class='left'>
-            <h5>SO, YOU WANT TO TRAVEL TO</h5>
-            <h1>SPACE</h1>
-            <p>
-              Let’s face it; if you want to go to space, you might as well
-              genuinely go to outer space and not hover kind of on the edge of it.
-              Well sit back, and relax because we’ll give you a truly out of this
-              world experience!
-            </p>
-          </div>
-          <div class='right'>
-            <div class='CTA'>
-              <h4>EXPLORE</h4>
-            </div>
+      <section class='hero'>
+        <div class='left'>
+          <h5>SO, YOU WANT TO TRAVEL TO</h5>
+          <h1>SPACE</h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div class='right'>
+          <div class='CTA'>
+            <h4>EXPLORE</h4>
           </div>
         </div>
       </section>
